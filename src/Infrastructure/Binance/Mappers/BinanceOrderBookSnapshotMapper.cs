51ab08bd6f723b0969558ac1fac02ad82acbcf64
ArturCode <a.khlapuk@bankvostok.com.ua>
2026-03-19 23:16:35 +0100

    add Binance data mapping

--- src/Infrastructure/Binance/Mappers/BinanceOrderBookSnapshotMapper.cs
+++ src/Infrastructure/Binance/Mappers/BinanceOrderBookSnapshotMapper.cs
@@ -9,14 +9,34 @@ namespace Infrastructure.Binance.Mappers
 {
     public class BinanceOrderBookSnapshotMapper : IOrderBookMapper, IBinanceSnapshotResultMapper
     {
-        public OrderBookSnapshot Map(ExternalOrderBookDto callResult)
-        {
-            throw new NotImplementedException();
-        }
-
         public ExternalOrderBookDto Map(WebCallResult<BinanceOrderBook> sdkResult)
         {
-            throw new NotImplementedException();
+            var bids = sdkResult.Data?.Bids
+                .Select(b => new ExternalOrderBookLevelDto { Price = b.Price, Quantity = b.Quantity })
+                .ToList() ?? new List<ExternalOrderBookLevelDto>();
+
+            var asks = sdkResult.Data?.Asks
+                .Select(a => new ExternalOrderBookLevelDto { Price = a.Price, Quantity = a.Quantity })
+                .ToList() ?? new List<ExternalOrderBookLevelDto>();
+
+            return new ExternalOrderBookDto
+            {
+                Bids = bids,
+                Asks = asks
+            };
+        }
+
+        public OrderBookSnapshot Map(ExternalOrderBookDto callResult)
+        {
+            return new OrderBookSnapshot
+            {
+                Bids = callResult.Bids
+                    .Select(b => new OrderBookLevel { Price = b.Price, Volume = b.Quantity })
+                    .ToList(),
+                Asks = callResult.Asks
+                    .Select(a => new OrderBookLevel { Price = a.Price, Volume = a.Quantity })
+                    .ToList()
+            };
         }
     }
 }
